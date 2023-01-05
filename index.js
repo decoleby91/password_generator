2ba90942e42d52e92f772756af4a9abb16d9bad8
James Coleby <32794776+decoleby91@users.noreply.github.com>
2023-01-05 23:46:30 +0000

    All characters were added

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 // Array of special characters to be included in password
 let specialCharacters = [
-
+    '@',
+    '%',
+    '+',
+    '\\',
+    '/',
+    "'",
+    '!',
+    '#',
+    '$',
+    '^',
+    '?',
+    ':',
+    ',',
+    ')',
+    '(',
+    '}',
+    '{',
+    ']',
+    '[',
+    '~',
+    '-',
+    '_',
+    '.'
 ];
