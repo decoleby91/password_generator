ec01243453b21d274793fb7679d282e4ae4a5e1e
James Coleby <32794776+decoleby91@users.noreply.github.com>
2023-01-05 23:53:55 +0000

    Added a comment to the file stating that I'll be adding a number of lowercase characters To be included in the passwords generated

--- index.js
+++ index.js
@@ -27,3 +27,5 @@ let specialCharacters = [
 
 // Array of numeric characters to be included in password
 let numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
