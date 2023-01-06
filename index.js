538b1282316771e7d49614f6a70ed2415e6136c6
James Coleby <32794776+decoleby91@users.noreply.github.com>
2023-01-06 00:42:34 +0000

    Created another variable 'passwordText' And given it a document.querySelector

--- index.js
+++ index.js
@@ -109,3 +109,4 @@ let generateBtn = document.querySelector('#generate');
 // Write password to the #password input
 function writePassword();
 let password = generatePassword();
+let passwordText = document.querySelector('#password');
