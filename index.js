3a257405973c5032ef4e41711cb6150a1a69446d
James Coleby <32794776+decoleby91@users.noreply.github.com>
2023-01-06 00:01:37 +0000

    Added a comment stating that I'll be adding A variable to use uppercase letters to Include in the password

--- index.js
+++ index.js
@@ -29,3 +29,33 @@ let specialCharacters = [
 let numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password
+let lowerCasedCharacters = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+];
+
+// Array of uppercase letters to include in the password
